84c8821dcaee46df4d8e8a5128ec826d3540a392
yutarokinoshita <1916013@s.asojuku.ac.jp>
2020-05-14 12:00:21 +0900

    データ更新

--- map.cpp
+++ map.cpp
@@ -2,7 +2,9 @@
 #include "main.h"
 #include "map.h"
 
-int mapImage;
+int mapImage;		// ƒ}ƒbƒv‰æ‘œ
+int cloudImage;	// ‰_‰æ‘œ
+CHARACTER cloud[CLOUD_MAX];
 int chipImage;
 XY mapPos;
 int map[20][20];
@@ -10,10 +12,25 @@ int map[20][20];
 void StageInit(void)
 {
 	mapImage = LoadGraph("image/backmap.png");
+	cloudImage = LoadGraph("image/cloud.png");
+	for (int clo = 0;clo < CLOUD_MAX;clo++)
+	{
+		cloud[clo].pos.x = 0 + CLOUD_SIZE_X * clo;
+		cloud[clo].pos.y = 32;
+	}
 }
 void StageDrawInit(void)
 {
 	DrawGraph(0, 0, mapImage, true);
+	for (int clo = 0;clo < CLOUD_MAX;clo++)
+	{
+		DrawGraph(cloud[clo].pos.x, cloud[clo].pos.y, cloudImage, true);
+		cloud[clo].pos.x--;
+		if (cloud[clo].pos.x <= -CLOUD_SIZE_X)
+		{
+			cloud[clo].pos.x = SCREEN_SIZE_X;
+		}
+	}
 	for (int x = 0;x < 25;x++)
 	{
 		for (int y = 4;y < 20;y++)
--- map.h
+++ map.h
@@ -4,6 +4,9 @@
 #define MAP_SIZE_Y_128
 #define CHIP_SIZE_X 32
 #define CHIP_SIZE_Y 32
+#define CLOUD_MAX	9
+#define CLOUD_SIZE_X 100
+#define CLOUD_SIZE_Y 64
 
 void StageInit(void);
 void StageDrawInit(void);
